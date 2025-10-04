2ecc9041bd70c26082bfdf635f601b1a3f62f971
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-10-04 20:34:34 +0600

    Add files via upload

---
+++ type_casting.py
@@ -0,0 +1,12 @@
+age= int(input())
+if age>= 18:
+    print("You are adult.")
+else:
+    print("You are child.")     
+    
+    
+weight=float(input())
+print(weight)
+print(type(weight)) 
+print(int(weight)) # int value ashbe
+   
