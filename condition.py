4c1b877f8004a3fc75b7509d69c1cb4c929df869
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-10-04 20:06:20 +0600

    Add files via upload

---
+++ condition.py
@@ -0,0 +1,5 @@
+rain=False
+if rain == True:
+    print("I will not go to Varsity.")
+else:
+    print("Will go to varsity.")    
---
+++ logical_operator.py
@@ -0,0 +1,15 @@
+#and, or, not
+
+print(10-5 == 5 and 20-15==5) # true
+print(10-4==6 or 12-2==1) # true
+print(not(10-6==5))# true
+
+
+marks=85
+if marks>=90 and marks <=100:
+    print("You will get two chocolates.") 
+elif marks>=80 and marks <=89:
+    print("You will get 1 chocolate.")
+else:
+    print("You will not get anything.")    
+        
